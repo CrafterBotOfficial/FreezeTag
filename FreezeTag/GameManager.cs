b0b1b5a655b3af5713908edc440a426df323ed26
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2024-02-10 21:29:49 -0500

    Project init

---
+++ FreezeTag/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+
+namespace FreezeTag;
+
+public class GameManager : GorillaGameManager
+{
+    public override GameModeType GameType() => GameModeType.Infection;
+    public override string GameModeName() => "FREEZE TAG";
+
+    public override void StartPlaying()
+    {
+
+    }
+
+    public override void OnSerializeRead(PhotonStream stream, PhotonMessageInfo info)
+    {
+    }
+
+    public override void OnSerializeWrite(PhotonStream stream, PhotonMessageInfo info)
+    {
+    }
+}
---
+++ FreezeTag/Main.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Utilla;
+
+namespace FreezeTag;
+
+[BepInPlugin("crafterbot.gorillatag.freezetag", "Freeze Tag", "1.0.0"), BepInDependency("org.legoandmars.gorillatag.utilla")]
+[ModdedGamemode("FREEZE TAG", "freeze tag", typeof(GameManager))]
+public class Main : BaseUnityPlugin
+{
+    private static Main instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static void Log(object message, LogLevel level = LogLevel.Info)
+    {
+        instance?.Logger.Log(level, message);
+    }
+}
